f8634ac88ab736cbab599515db4525d2afbd66af
Arden Yan <148033490+ardenyan13@users.noreply.github.com>
2024-10-28 01:13:41 -0700

    Complete setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="arden_yan_calculator_7085",
+    version="0.1.0",
+    author="Arden Yan",
+    author_email="arden.yan@sjsu.edu",
+    description="An advanced calculator package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ardenyan13/arden_calculator",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.12',
+)
